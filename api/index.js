d0d7f4cb2b56177e6657592f165b48b2fec5d99b
Ivaylo Stoyanov <ipstoyanov20@codingburgas.bg>
2024-04-13 22:07:09 +0300

    Update index

--- api/index.js
+++ api/index.js
@@ -45,58 +45,58 @@ app.get("/api", async (req, res) => {
 	await page.goto("https://menu.codingburgas.bg", {
 		waitUntil: "networkidle2",
 	});
+	res.send(await page.title());
+	// const Office = await page.waitForSelector("button.btn.btn-danger");
+	// await Office.click();
 
-	const Office = await page.waitForSelector("button.btn.btn-danger");
-	await Office.click();
+	// const emailInput = await page.waitForSelector("input#i0116");
+	// await emailInput.type(process.env.M_USR);
 
-	const emailInput = await page.waitForSelector("input#i0116");
-	await emailInput.type(process.env.M_USR);
+	// const next = await page.waitForSelector(
+	// 	"input#idSIButton9.win-button.button_primary.button.ext-button.primary.ext-primary",
+	// );
+	// await next.click();
 
-	const next = await page.waitForSelector(
-		"input#idSIButton9.win-button.button_primary.button.ext-button.primary.ext-primary",
-	);
-	await next.click();
+	// const passInput = await page.waitForSelector("input#i0118");
+	// await passInput.type(process.env.M_PWD);
 
-	const passInput = await page.waitForSelector("input#i0118");
-	await passInput.type(process.env.M_PWD);
+	// setTimeout(async () => {
+	// 	await page.keyboard.press("Enter");
+	// }, 1000);
 
-	setTimeout(async () => {
-		await page.keyboard.press("Enter");
-	}, 1000);
+	// await page
+	// 	.waitForSelector("div.text-block-body.overflow-hidden.no-margin-top")
+	// 	.then(async () => {
+	// 		await page.keyboard.press("Enter");
+	// 	});
 
-	await page
-		.waitForSelector("div.text-block-body.overflow-hidden.no-margin-top")
-		.then(async () => {
-			await page.keyboard.press("Enter");
-		});
+	// new Promise((resolve) => setTimeout(resolve, 2000));
 
-	new Promise((resolve) => setTimeout(resolve, 2000));
-
-	await page.waitForSelector("a.btn.btn-primary", {
-		timeout:3500,
-	});
+	// await page.waitForSelector("a.btn.btn-primary", {
+	// 	timeout:3500,
+	// });
 	
-	const stayIn = await page.$$("a.btn.btn-primary");
+	// const stayIn = await page.$$("a.btn.btn-primary");
 	
 
-	for (let i = 0; i <= stayIn.length; i++) {
+	// for (let i = 0; i <= stayIn.length; i++) {
 
-		await page.waitForSelector("a.btn.btn-primary");
-		const checkbox = await page.$("a.btn.btn-primary");
-		await checkbox.click();
+	// 	await page.waitForSelector("a.btn.btn-primary");
+	// 	const checkbox = await page.$("a.btn.btn-primary");
+	// 	await checkbox.click();
 
-		// Wait for the elements you want to interact with in the same context
-		await page.waitForSelector("input#AvailablePackets_0__Selected");
-		const packet = await page.$("input#AvailablePackets_0__Selected");
-		await packet.click();
+	// 	// Wait for the elements you want to interact with in the same context
+	// 	await page.waitForSelector("input#AvailablePackets_0__Selected");
+	// 	const packet = await page.$("input#AvailablePackets_0__Selected");
+	// 	await packet.click();
 
-		await page.waitForSelector("input.btn.btn-primary");
-		const reserve = await page.$("input.btn.btn-primary");
-		await reserve.click();
-	}
+	// 	await page.waitForSelector("input.btn.btn-primary");
+	// 	const reserve = await page.$("input.btn.btn-primary");
+	// 	await reserve.click();
+	// }
 
-	res.send("I Am in");
-	await browser.close();
+	// res.send("I Am in");
+	// await browser.close();
 } catch (error) {
 	console.log(error);
 	return null;
