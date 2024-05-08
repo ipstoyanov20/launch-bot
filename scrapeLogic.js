ac26d07d6bbe755a52fd0c6a3f0c3ec8c6b316c9
Ivaylo Stoyanov <ipstoyanov20@codingburgas.bg>
2024-05-08 18:19:27 +0300

    make old

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -2,78 +2,53 @@ import puppeteer from "puppeteer"
 import {config} from "dotenv"
 config();
 
+
 const scrapeLogic = async (res) => {
+  
   const browser = await puppeteer.launch({
-		executablePath:
-			process.env.NODE_ENV === "production"
-      ? process.env.PUPPETEER_EXECUTABLE_PATH
-      : puppeteer.executablePath(),
-      headless: process.env.NODE_ENV === "production" ? true : false,
-		defaultViewport: null,
-		args: ["--no-sandbox", "--disable-dev-shm-usage"],
-	});
-    try {
-			const page = await browser.newPage();
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    headless: false,
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+  try {
+    const page = await browser.newPage();
 
-			// Navigate to the page that will perform the tests.
-			await page.goto("https://menu.codingburgas.bg", {
-				waitUntil: "networkidle2",
-			});
+    await page.goto("https://developer.chrome.com/");
 
-			const Office = await page.waitForSelector("button.btn.btn-danger");
-			await Office.click();
+    // Set screen size
+    await page.setViewport({ width: 1080, height: 1024 });
 
-			const emailInput = await page.waitForSelector("input#i0116");
-			await emailInput.type(process.env.M_USR);
+    // Type into search box
+    await page.type(".search-box__input", "automate beyond recorder");
 
-			const next = await page.waitForSelector(
-				"input#idSIButton9.win-button.button_primary.button.ext-button.primary.ext-primary",
-			);
-			await next.click();
+    // Wait and click on first result
+    const searchResultSelector = ".search-box__link";
+    await page.waitForSelector(searchResultSelector);
+    await page.click(searchResultSelector);
 
-			const passInput = await page.waitForSelector("input#i0118");
-			await passInput.type(process.env.M_PWD);
+    // Locate the full title with a unique string
+    const textSelector = await page.waitForSelector(
+      "text/Customize and automate"
+    );
+    const fullTitle = await textSelector.evaluate((el) => el.textContent);
 
-			setTimeout(async () => {
-				await page.keyboard.press("Enter");
-			}, 1000);
-
-			await page
-				.waitForSelector("div.text-block-body.overflow-hidden.no-margin-top")
-				.then(async () => {
-					await page.keyboard.press("Enter");
-				});
-
-			new Promise((resolve) => setTimeout(resolve, 2000));
-
-			await page.waitForSelector("a.btn.btn-primary", {
-				timeout: 3500,
-			});
-
-			const stayIn = await page.$$("a.btn.btn-primary");
-
-			for (let i = 0; i <= stayIn.length; i++) {
-				await page.waitForSelector("a.btn.btn-primary");
-				const checkbox = await page.$("a.btn.btn-primary");
-				await checkbox.click();
-
-				// Wait for the elements you want to interact with in the same context
-				await page.waitForSelector("input#AvailablePackets_0__Selected");
-				const packet = await page.$("input#AvailablePackets_0__Selected");
-				await packet.click();
-
-				await page.waitForSelector("input.btn.btn-primary");
-				const reserve = await page.$("input.btn.btn-primary");
-				await reserve.click();
-			}
-			await browser.close();
-			res.send(`Here we are:`);
-		} catch (e) {
-			console.error(e);
-			res.send(`Something went wrong while running Puppeteer: ${e}`);
-		} finally {
-			await browser.close();
-			res.send(`Closed:`);
-		}
-}
+    // Print the full title
+    const logStatement = `The title of this blog post is ${fullTitle}`;
+    console.log(logStatement);
+    res.send(logStatement);
+  } catch (e) {
+    console.error(e);
+    res.send(`Something went wrong while running Puppeteer: ${e}`);
+  } finally {
+    await browser.close();
+  }
+};
 export {scrapeLogic};
